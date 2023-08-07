7c6b96c4a5d4d267a4194827939a04006ed49ca6
daniel khen <danielkhen20@gmail.com>
2023-08-07 13:54:08 +0300

    Auto-Commit:

--- main.tf
+++ main.tf
@@ -14,19 +14,27 @@ locals {
   private_endpoint_subresource = "registry"
 }
 
+module "private_dns_zone" {
+  source   = "github.com/danielkhen/private_dns_zone_module"
+  for_each = var.private_dns_enabled ? [true] : []
+
+  name                = var.dns_name
+  resource_group_name = var.resource_group_name
+  vnet_links          = var.vnet_links
+}
+
 module "hub_acr_private_endpoint" {
-  source = "github.com/danielkhen/private_endpoint_module"
-  count  = var.private_endpoint_enabled ? 1 : 0
+  source   = "github.com/danielkhen/private_endpoint_module"
+  for_each = var.private_endpoint_enabled ? [true] : []
 
   name                = var.private_endpoint_name
   location            = var.location
   resource_group_name = var.resource_group_name
   private_dns_enabled = var.private_dns_enabled
-  dns_name            = var.dns_name
+  private_dns_zone_id = var.private_dns_enabled ? module.private_dns_zone[0].id : null
   log_analytics_id    = var.log_analytics_id
 
   resource_id      = azurerm_container_registry.acr.id
   subresource_name = local.private_endpoint_subresource
   subnet_id        = var.private_endpoint_subnet_id
-  vnet_links       = var.vnet_links
 }
